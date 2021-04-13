9f780e20123c6cc8c5d5d0046f2b021117d557a9
Daojiamoxia <42091694+Daojiamoxia@users.noreply.github.com>
2021-04-13 09:15:41 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+var snake=document.getElementById("snake")
+document.onkeydown = keyDown;
+document.write(snake.margin-left)
+function keyDown(event){
+	var event = event || window.event;
+	switch(event.keyCode){
+		case 37:
+			/*snake.margin-left=snake.margin-left-5+"px";*/
+			break;
+		case 39:
+			snake.left=snake.offsetLeft+5+"px";
+			break;
+		case 38:
+			snake.top=snake.offsetTop-5+"px";
+			break;
+		case 40:
+			snake.top=snake.offsetTop+5+"px";
+			break;
+	}
 
+}
